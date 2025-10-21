6cbd7a098883a08a964447cde1a9893603479231
maddiehaaga <madelynhaaga12@gmail.com>
2025-10-21 14:31:46 -0400

    Add Orders functionality with order listing and detail views

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home/Home';
 import Products from './Products/Products';
+import Orders from './Orders/Orders';
 import { Link } from 'react-router-dom';
 function App() {
   const openMenu = () => {
@@ -32,6 +33,7 @@ function App() {
             <div className="header-links">
                 <Link to="/">Home</Link>
                 <Link to ="/catalog">Catalog</Link>
+                <Link to="orders">Orders</Link>
             </div>
         </header>
         <aside className="sidebar">
@@ -51,6 +53,8 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/catalog" element={<Products />} />
+                <Route path="/orders" element={<Orders />} />
+                <Route path="/order/:id" element={<OrderDetail />} /></Route>
             </Routes>
             </main>
             
